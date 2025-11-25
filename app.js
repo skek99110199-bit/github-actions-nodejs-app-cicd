7267c8b6cd16388bde9ac5f805727b72ee522572
singihoon <skek99110199@gmail.com>
2025-11-26 01:06:51 +0900

    update app.js file

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Helloo, World!!!!\n');
+  res.end('Hello, World!!!!\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server is  running at http://${hostname}:${port}/`);
+  console.log(`Node JS Server is  running at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
